f86e1b1432477fd24bffc9d1cec03eaaa736a176
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-05-05 15:31:24 +0200

    Added: rwd rows, follow status

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {spotifyApi} from "../services/ApiClient";
-import axios, { AxiosResponse } from "axios";
+import "../styles/elements/_top-artists.scss";
 
 
 
@@ -28,51 +28,107 @@ import axios, { AxiosResponse } from "axios";
 
 export const TopArtists = (props: {userName: string}) :JSX.Element => {
     const [topArtist, setTopArtist] = useState<Array<any>>([]);
+    const [isFollowed, setIsFollowed] = useState <Array<boolean>>  ([true])
     let [offset, setOffset] = useState<number>(1);
     const LIMIT = 6;
-    const INITIAL_URL = 'https://api.spotify.com/v1/me/top/artists'
 
     useEffect(() => {
         updateTopArtistShort()
-
+        getFollowShort()
     },[]);
 
     async function updateTopArtistLong() {
         let responseData  = await fetchTopArtistsLong();
         setOffset(0)
         setTopArtist(responseData)
-        function fetchTopArtistsLong () {
-            return spotifyApi.getMyTopArtistsLong().then((response) => {
-                return response.items
-            })
-        }
+
+    }
+    function fetchTopArtistsLong () {
+        return spotifyApi.getMyTopArtistsLong().then((response) => {
+            return response.items
+        })
     }
     async function updateTopArtist() {
         let responseData = await fetchTopArtists();
         setTopArtist(responseData)
         setOffset(0)
-        function fetchTopArtists () {
-            return spotifyApi.getMyTopArtists().then((response) => {
-                return response.items
-            })
-        }
+
+    }
+    function fetchTopArtists () {
+        return spotifyApi.getMyTopArtists().then((response) => {
+            return response.items
+        })
     }
     async function updateTopArtistShort() {
         let responseData = await fetchTopArtistsShort();
         setTopArtist(responseData)
         setOffset(0)
-        function fetchTopArtistsShort () {
-            return spotifyApi.getMyTopArtistsShort().then((response) => {
-                return response.items
+
+    }
+    function fetchTopArtistsShort () {
+        return spotifyApi.getMyTopArtistsShort().then((response) => {
+            return response.items
+        })
+    }
+
+    async function getFollowShort () {
+        // let [artist] = await Promise.all([fetchTopArtistsShort(), fetchTopArtists(), fetchTopArtistsLong ()]);
+        let artist = await fetchTopArtistsShort()
+        let artistId = artist.map(item => {return item.id})
+        function fetchFollowedArtistsShort() {
+            return spotifyApi.isFollowingArtists(artistId).then(response => {
+                return response
             })
         }
+        let responseData = await fetchFollowedArtistsShort()
+        setIsFollowed(responseData)
+    }
+
+    async function getFollow () {
+        let artist = await fetchTopArtists()
+        let artistId = artist.map(item => {return item.id})
+        function fetchFollowedArtists() {
+            return spotifyApi.isFollowingArtists(artistId).then(response => {
+                return response
+            })
+        }
+        let responseData = await fetchFollowedArtists()
+        setIsFollowed(responseData)
+    }
+    async function getFollowLong () {
+        let artist = await fetchTopArtistsLong()
+        let artistId = artist.map(item => {return item.id})
+        function fetchFollowedArtistsLong() {
+            return spotifyApi.isFollowingArtists(artistId).then(response => {
+                return response
+            })
+        }
+        let responseData = await fetchFollowedArtistsLong()
+        setIsFollowed(responseData)
     }
 
 
 
 
-    console.log(topArtist[0])
-    function nextArtists() {
+
+const  displayIsFollowed =() => {
+
+       return isFollowed?.map(item => item.toString())
+}
+
+    function updateCardsShort() {
+        updateTopArtistShort()
+        getFollowShort()
+    }
+    function updateCards(){
+        updateTopArtist();
+        getFollow()
+    }
+    function updateCardsLong() {
+        updateTopArtistLong()
+        getFollowLong()
+    }
+    function nextPage() {
         if(topArtist.length <= offset+LIMIT) {
             return
         }
@@ -80,7 +136,7 @@ export const TopArtists = (props: {userName: string}) :JSX.Element => {
         const newOffset = offset+LIMIT;
         setOffset(newOffset)
     }
-    function previousArtist() {
+    function previousPage() {
         if (offset-LIMIT < 0) {
             return
         }
@@ -89,21 +145,33 @@ export const TopArtists = (props: {userName: string}) :JSX.Element => {
         setOffset(newOffset)
     }
 
+
     function artistCards () {
+        //Nie tu
         return (
-            <div className="card__container flex flex-wrap sm:flex-wrap overflow-x-auto sm:justify-center ">
+            <div className="card__container flex flex-wrap  sm:flex-wrap sm:justify-center ">
                 {topArtist.slice(offset,offset+LIMIT).map((item, index)=> {
                     return (
 
-                        <div className="card max-w-sm rounded  shadow-lg m-2 bg-white ">
-                            <img className="w-full" src={item.images[0].url} alt="Sunset in the mountains"/>
+                        <div
+                            key={index}
+                            className="card rounded  sm:w-1/4 md:w-1/4  shadow-lg m-2 bg-white ">
+                            <img className="" src={item.images[0].url} alt="Sunset in the mountains"/>
                             <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2">  {index+1+"."}{item.name} </div>
-                                <div className="card__followers font-hairline text-xs">Followed by: {item.followers.total}</div>
-                                <p className="text-gray-700 text-base">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                                    Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                <div className="font-bold text-xl mb-2">  {index+1+"."} {item.name} {displayIsFollowed()?.[index]}</div>
+                                <div className="card__followers font-bold font-hairline text-xs sm:text-sm ">Followed by: <span className="font-hairline"> {item.followers.total} people </span></div>
+                                <div className="card__popularity font-bold font-hairline text-xs sm:text-sm"> Popularity: <span className="font-hairline">{item.popularity} /100 </span></div>
+                                <div className="card__popularity font-bold text-xs sm:text-sm"> Id: <span className="font-hairline"> {item.id}</span></div>
+
+                                <div className="flex">
+                                <p className="text-base  mt-10">
+                                    <a href={item.external_urls.spotify}> <button className="text-center bg-purple-700 rounded px-5 py-2 text-sm mt-1 font-semibold text-white mr-2"> See at Spotify </button></a>
                                 </p>
+                                <p className="text-base  sm:ml-auto mt-10">
+                                    <a href={item.external_urls.spotify}> <button className="text-center bg-purple-700 rounded px-5 py-2 text-sm mt-1 font-semibold text-white mr-2"> Follow </button></a>
+                                </p>
+                            </div>
+
                             </div>
                             <div className="px-6 py-4">
                                 {item.genres[0] ? <span
@@ -142,17 +210,17 @@ export const TopArtists = (props: {userName: string}) :JSX.Element => {
     <div className="mb-5 text-center">
         <div className="inline-flex">
             <button
-                onClick={() =>updateTopArtistShort()}
+                onClick={() =>updateCardsShort() }
                 className="bg-gray-400 hover:bg-gray-500 text-gray-800 font-bold rounded-l  py-1 px-2 m-1">
                 4 weeks
             </button>
             <button
-                onClick={() =>updateTopArtist()}
+                onClick={() =>updateCards()}
                 className="bg-gray-400 hover:bg-gray-500 text-gray-800 font-bold py-1 px-2 m-1">
                6 months
             </button>
             <button
-                onClick={() => updateTopArtistLong()}
+                onClick={() => updateCardsLong()}
                 className="bg-gray-400 hover:bg-gray-500 text-gray-800 font-bold py-1 px-2 rounded-r m-1">
                Beggining!
             </button>
@@ -160,16 +228,16 @@ export const TopArtists = (props: {userName: string}) :JSX.Element => {
     </div>
             {artistCards()}
 
-            <div className="flex  justify-center mt-5 mb-5">
+            <div className="flex justify-center mt-5 mb-5">
                 <button
-                    className= {offset === 0 ? "text-white fill-current  mr-1 px-2 py-1" :"text-white border fill-current  mr-1 px-2 py-1"}
-                    onClick={() => previousArtist()}
+                    className= {offset === 0 ? "text-white fill-current  mr-1 px-2 py-1" :"text-white border font-bold fill-current  mr-1 px-2 py-1"}
+                    onClick={() => previousPage()}
                 >
-                    {offset === 0 ? "Click on right button!" : "Move Back" }
+                    {offset === 0 ? "Click ----> " : "Move Back" }
                 </button>
                 <button
-                    className={offset+LIMIT> topArtist.length ? "text-white fill-current  mr-1 px-2 py-1" : "text-white border fill-current  ml-1 px-2 py-1"}
-                    onClick={() => nextArtists() }
+                    className={offset+LIMIT> topArtist.length ? "text-white fill-current  mr-1 px-2 py-1 " : "text-white font-bold border fill-current  ml-1 px-2 py-1"}
+                    onClick={() => nextPage() }
                 >
                     {offset+LIMIT< topArtist.length ? "See More" : "Chceck other list!" }
                 </button>
