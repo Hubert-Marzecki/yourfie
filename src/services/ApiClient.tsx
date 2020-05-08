c3248230b548e8418c870eccb8e439dfeeddeb17
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-05-08 13:37:12 +0200

    separated fetch functions from topArtists

--- src/services/ApiClient.tsx
+++ src/services/ApiClient.tsx
@@ -25,3 +25,49 @@ export const token = params.access_token ;
 if (token) {
   spotifyApi.setAccessToken(token);
 }
+
+// fetch top artist from 4weeks/6msc/beginning
+export function fetchTopArtistsLong () {
+  return spotifyApi.getMyTopArtistsLong().then((response) => {
+    return response.items
+  })
+}
+export  function fetchTopArtists () {
+  return spotifyApi.getMyTopArtists().then((response) => {
+    return response.items
+  })
+}
+export   function fetchTopArtistsShort () {
+  return spotifyApi.getMyTopArtistsShort().then((response) => {
+    return response.items
+  })
+}
+
+// check is artists follow
+export  function fetchFollowedArtistsShort(artistId : string[]) {
+  return spotifyApi.isFollowingArtists(artistId).then(response => {
+    return response
+  })
+}
+export  function fetchFollowedArtists(artistId : string[]) {
+  return spotifyApi.isFollowingArtists(artistId).then(response => {
+    return response
+  })
+}
+export  function fetchFollowedArtistsLong(artistId : string[]) {
+  return spotifyApi.isFollowingArtists(artistId).then(response => {
+    return response
+  })
+}
+
+// push follow / unfollow actions
+export function pushFollowArtist(id: string) {
+  return spotifyApi.followArtists([id]).then(response => {
+    return response
+  })
+}
+export  function pushUnfollowArtist(id:string) {
+  return spotifyApi.unfollowArtists([id]).then(response => {
+    return response
+  })
+}
